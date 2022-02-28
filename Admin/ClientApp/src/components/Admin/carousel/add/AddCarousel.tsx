0f543d3be115875430f89e0d42a2997ea89efb47
Hiren Paghadal <hirenpaghadal031@gmail.com>
2022-02-28 15:53:26 +0530

    Add Carousel validation

--- Admin/ClientApp/src/components/Admin/carousel/add/AddCarousel.tsx
+++ Admin/ClientApp/src/components/Admin/carousel/add/AddCarousel.tsx
@@ -1,27 +1,73 @@
 import { InboxOutlined, UnorderedListOutlined } from "@ant-design/icons";
-import { Button, Card, Col, Form, Input, Row, Typography, Upload } from "antd";
-import React, { FC } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  Input,
+  message,
+  Row,
+  Typography,
+  Upload,
+} from "antd";
+import React, { FC, useState } from "react";
 import { useHistory } from "react-router";
 import { navigate } from "../../../../common/navigation";
 import { AdminRoutesConstant } from "../../../../routes/AdminRoutes";
 import "./AddCarousel.scss";
 
+const { Dragger } = Upload;
 const { Title } = Typography;
 
 export const AddCarousel: FC = () => {
   const history = useHistory();
 
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 4 },
+  const props = {
+    name: "file",
+    multiple: true,
+    action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+    onChange(info: { file: { name?: any; status?: any }; fileList: any }) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
     },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 20 },
+    onDrop(e: { dataTransfer: { files: any } }) {
+      console.log("Dropped files", e.dataTransfer.files);
     },
   };
 
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 5 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 19 },
+    },
+  };
+  const [form] = Form.useForm();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onFinish = () => {
+    setIsLoading(true);
+    form
+      .validateFields()
+      .then(async (values) => {
+        console.log("values", values);
+      })
+      .catch((info) => {
+        // console.log(info);
+        setIsLoading(false);
+        console.log("Validate Failed:", info);
+      });
+  };
   return (
     <div className="addcarousel">
       <Row justify="space-between" align="middle">
@@ -40,10 +86,7 @@ export const AddCarousel: FC = () => {
             onClick={() =>
               navigate(
                 history,
-                AdminRoutesConstant.AdminPages.CarouselList.path
-              )
-            }
-          >
+                AdminRoutesConstant.AdminPages.CarouselList.path)}>
             Carousel List
           </Button>
         </Col>
@@ -51,26 +94,42 @@ export const AddCarousel: FC = () => {
       <Row>
         <Col xs={24} md={12}>
           <Card>
-            <Form {...formItemLayout}>
-              <Form.Item label="Title">
+            <Form {...formItemLayout} form={form} onFinish={onFinish}>
+              <Form.Item
+                label="Title"
+                name="title"
+                rules={[{ required: true, message: "Please input your Title" }]}
+              >
                 <Input placeholder="Title" />
               </Form.Item>
-              <Form.Item label="Description">
+              <Form.Item
+                label="Description"
+                name="description"
+                rules={[
+                  { required: true, message: "Please input your Description" },
+                ]}
+              >
                 <Input.TextArea placeholder="Description" />
               </Form.Item>
-              <Form.Item label="Image">
-                <Form.Item name="image" valuePropName="file" noStyle>
-                  <Upload.Dragger name="imagefile">
+              <Form.Item
+                label="Image"
+                name="image"
+                rules={[
+                  { required: true, message: "Please choose your image" },
+                ]}
+              >
+                <Form.Item name="image" rules={[{ required: false }]} noStyle>
+                  <Dragger {...props} name="imagefile">
                     <p className="ant-upload-drag-icon">
                       <InboxOutlined />
                     </p>
                     <p className="ant-upload-text">
                       Click or drag file to this area to upload
                     </p>
-                  </Upload.Dragger>
+                  </Dragger>
                 </Form.Item>
               </Form.Item>
-              <Form.Item wrapperCol={{ span: 14, offset: 4 }}>
+              <Form.Item wrapperCol={{ span: 13, offset: 5 }}>
                 <Button
                   type="primary"
                   htmlType="submit"
