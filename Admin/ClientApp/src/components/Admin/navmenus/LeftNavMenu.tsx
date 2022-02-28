06c748686d85627a57845fa474b560062aaf2995
Hiren Paghadal <hirenpaghadal031@gmail.com>
2022-02-28 18:00:25 +0530

    Left menu collapsible

--- Admin/ClientApp/src/components/Admin/navmenus/LeftNavMenu.tsx
+++ Admin/ClientApp/src/components/Admin/navmenus/LeftNavMenu.tsx
@@ -1,8 +1,10 @@
+import { AppstoreOutlined } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
 import React, { FC, useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router";
 import { navigate } from "../../../common/navigation";
 import { AdminRoutesConstant } from "../../../routes/AdminRoutes";
+import "./LeftNavMenu.scss";
 
 const { Sider } = Layout;
 
@@ -10,16 +12,26 @@ export const LeftNavMenu: FC = () => {
   const location = useLocation();
   const history = useHistory();
 
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const onCollapse = (collapsed: any) => {
+    setIsCollapsed(collapsed);
+  };
+
   return (
-    <Sider width={200} className="left_menu">
-      <Menu mode="inline" defaultSelectedKeys={[location.pathname.split("/")[2]]}>
+    <Sider collapsible collapsed={isCollapsed} onCollapse={onCollapse} theme="light">
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={[location.pathname.split("/")[2]]}
+      >
         <Menu.Item
           key="carousel"
+          icon={<AppstoreOutlined />}
           onClick={() =>
             navigate(history, AdminRoutesConstant.AdminPages.CarouselList.path)
           }
         >
-          Carousel
+         Carousel
         </Menu.Item>
       </Menu>
     </Sider>
