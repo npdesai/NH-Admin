4b197abc1a988ab843b9bdaab5c3bb74a4c669e4
Hiren Paghadal <hirenpaghadal031@gmail.com>
2022-03-02 14:25:51 +0530

    Carousel list data binding

--- Admin/ClientApp/src/components/Admin/carousel/list/CarouselList.tsx
+++ Admin/ClientApp/src/components/Admin/carousel/list/CarouselList.tsx
@@ -1,20 +1,77 @@
 import { PlusCircleOutlined } from "@ant-design/icons";
-import { Button, Col, Row, Typography } from "antd";
-import React, { FC } from "react";
+import { Button, Col, Row, Table, Typography } from "antd";
+import React, { FC, useEffect, useState } from "react";
 import { useHistory } from "react-router";
+import { CarouselClient } from "../../../../clients/api.generated.clients";
 import { navigate } from "../../../../common/navigation";
 import { AdminRoutesConstant } from "../../../../routes/AdminRoutes";
 import "./CarouselList.scss";
 
 const { Title } = Typography;
-
+const onchange = () => {};
 export const CarouselList: FC = () => {
   const history = useHistory();
+  const [carouselData, setCarouselData] = useState([]);
+
+  useEffect(() => {
+    new CarouselClient()
+      .getCarousels()
+      .then((response) => {
+        let carouselTableData: any = [];
+        response.data &&
+          response.data.map((carousel) => {
+            carouselTableData.push({
+              key: carousel.id,
+              title: carousel.title,
+              description: carousel.description,
+              image: carousel.image,
+              isActive: carousel.isActive,
+            });
+          });
+        setCarouselData(carouselTableData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const carouselTableColumns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      width: "15%",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      width: "20%",
+      render: (_image: any) => <img height={40} src={_image} />,
+    },
+    {
+      title: "IsActive",
+      dataIndex: "isActive",
+      width: "10%",
+      render: (_Checkbox: any) => (
+        <input type="checkbox" onChange={onchange} checked={_Checkbox} />
+      ),
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      width: "10%",
+      render: (_button: any) => <Button>Edit</Button>,
+    },
+  ];
   return (
     <div className="carousellist">
       <Row justify="space-between" align="middle">
         <Col xs={12}>
-          <Title level={3} className="carousellist_pagetitle">Carousel List</Title>
+          <Title level={3} className="carousellist_pagetitle">
+            Carousel List
+          </Title>
         </Col>
         <Col xs={12} className="carousellist_action">
           <Button
@@ -29,6 +86,11 @@ export const CarouselList: FC = () => {
           >
             Add Carousel
           </Button>
+        </Col>
+      </Row>
+      <Row className="carousellist_table">
+        <Col xs={24}>
+          <Table dataSource={carouselData} columns={carouselTableColumns} />
         </Col>
       </Row>
     </div>
